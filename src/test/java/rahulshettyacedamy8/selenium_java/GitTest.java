682150ef867b0ed1c98233e1c6ee97060d141b07
RAJU PONNAM <raju.ponnam554@gmail.com>
2025-06-21 17:59:08 +0530

    This is commit from asian time zone

---
+++ src/test/java/rahulshettyacedamy8/selenium_java/GitTest.java
@@ -0,0 +1,14 @@
+package rahulshettyacedamy8.selenium_java;
+
+import org.testng.annotations.Test;
+
+public class GitTest {
+	
+	@Test
+	
+	public void test()
+	{
+		System.out.println("hello");
+	}
+
+}
